9da6cc0bf3cf04c0a5f1cd04a6ae267da2479f9d
JoSchaer <joggl.bimbum@web.de>
2019-03-30 13:57:49 +0100

    login files and sockets

---
+++ chat.js
@@ -0,0 +1,38 @@
+//Connection to Server
+var socket = io();
+
+
+
+
+
+
+
+
+
+/*
+//Elements from Login
+var username = document.getElementById('username');
+    loginBtn = document.getElementById('login');
+
+
+//Elements from Chat
+/*
+loginBtn.addEventListener('click', function () {
+   socket.emit()
+});
+
+
+/*
+$(function () {
+    var socket = io();
+    $('form').submit(function (e) {
+        e.preventDefault(); // prevents page reloading
+        socket.emit('chat message', $('#m').val());
+        $('#m').val('');
+        return false;
+    });
+    socket.on('chat message', function (msg) {
+        $('#messages').append($('<li>').text(msg));
+    });
+});
+*/
--- index.js
+++ index.js
@@ -1,29 +1,15 @@
+//Connection Settings
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/chat.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
-        io.emit('chat message', msg);
-    });
-});
-
-io.on('connection', function(socket){
-
-    console.log('Yay, connection was recorded')
-
-    //emit message to all front-end clients
-    io.emit('chat message', 'Some **** Joined the gangbang Party');
-
-    //handling disconnects
-    socket.on('disconnect', function() {
-        io.emit('chat message', '**** left early');
-    });
-
+    console.log('a user connected');
 });
 
 http.listen(3000, function(){
