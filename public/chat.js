a975f239b85c711e36d9218432eb8bbbd9813b61
JoSchaer <joggl.bimbum@web.de>
2019-03-30 15:07:38 +0100

    deleted unused code from old examlpe

--- public/chat.js
+++ public/chat.js
@@ -2,30 +2,13 @@
 var socket = io.connect('http://localhost:3000');
 
 
-/*
 //Elements from Login
 var username = document.getElementById('username');
-    loginBtn = document.getElementById('login');
+loginBtn = document.getElementById('login');
 
 
 //Elements from Chat
-/*
 loginBtn.addEventListener('click', function () {
-   socket.emit()
+    socket.emit()
 });
 
-
-/*
-$(function () {
-    var socket = io();
-    $('form').submit(function (e) {
-        e.preventDefault(); // prevents page reloading
-        socket.emit('chat message', $('#m').val());
-        $('#m').val('');
-        return false;
-    });
-    socket.on('chat message', function (msg) {
-        $('#messages').append($('<li>').text(msg));
-    });
-});
-*/
