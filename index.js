a69a964930c313ca7385bb2f1185dbee70161890
JoSchaer <joggl.bimbum@web.de>
2019-03-31 04:00:11 +0200

    GUI Ready only functionality is missed

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ var server = app.listen(3000, function () {
     console.log(' listening on Port 3000')
 })
 
-
 app.use(express.static('public'));
-
 
 var io = socket(server);
 
+
+var userList = [];
+
 io.on('connection', function (socket) {
     socket.on('chat', function (data) {
+        socket.emit(data.username + ' joined the Chat')
         console.log(data.username + ' connectet to the Chat');
-    })
+        userList.push(socket);
+
+        console.log(userList.length);
+    });
+
+    io.on('chat message', function (msg) {
+       io.sockets.emit('chat message', msg);
+    });
 });
 
--- public/chat.js
+++ public/chat.js
@@ -3,21 +3,30 @@ var socket = io.connect('http://localhost:3000');
 
 //Elements from Login
 var username = document.getElementById('username');
-loginBtn = document.getElementById('login');
+var loginBtn = document.getElementById('login');
+var messages = document.getElementById('messages');
+var sendBtn = document.getElementById('sending');
+var message = document.getElementById('m');
+
 
 
 //Elements from Chat
 loginBtn.addEventListener('click', function () {
-    if (username.value != null) {
-        socket.emit('chat', {
-            username: username.value
-        });
-        window.location.href = "chat.html";
-    }
-    else{
-        alert("Please enter an Username");
-    }
+    socket.emit('chat', {
+        username: username.value
+    });
+    window.location.href = "chat.html";
+    document.getElementById('userNamePlace').innerText = username.value;
+});
 
-})
-;
+sendBtn.addEventListener('click',function () {
+    socket.emit('chat message',{
+        username : username.value,
+        message: message.value
+    });
+});
+
+socket.on('chat message', function (msg) {
+    messages.innerHTML += '<p><strong>'+ msg.username+':</strong>'+msg.message+'</p>';
+});
 
